d5ac9afa566fbee25c1d9b8f368a553b8dde9350
Brandon Ngo <brandonngo56@gmail.com>
2023-02-09 21:32:16 -0500

    use moment.js library to change seconds to minutes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Break from './components/Break';
+import Session from './components/Session';
+
+function App() {
+  return (
+    <div className="App">
+      <Break />
+      <Session />
+    </div>
+  );
+}
+
+export default App;
