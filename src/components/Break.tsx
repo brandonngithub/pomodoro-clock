8fce9ceed6bb7cfa61edb90266cc91bac727436f
Brandon Ngo <brandonngo56@gmail.com>
2023-02-14 17:44:10 -0500

    fix bug

---
+++ src/components/Break.tsx
@@ -0,0 +1,47 @@
+import moment from "moment";
+import React from "react";
+import { BreakSessionContainer } from "../ui/BreakSessionUi";
+import { BreakSessionLabel } from "../ui/BreakSessionUi";
+import { BreakSessionTime } from "../ui/BreakSessionUi";
+import { PlusMinusButtonContainer } from "../ui/BreakSessionUi";
+import { PlusMinusButton } from "../ui/BreakSessionUi";
+
+const Break: React.FC<Props> = (props) => {
+  const {
+    breakLength,
+    decrementBreakLengthByOneMinute,
+    incrementBreakLengthByOneMinute,
+  } = props;
+
+  const breakLengthInMinutes = moment.duration(breakLength, "s").minutes();
+  return (
+    <BreakSessionContainer>
+      <BreakSessionLabel id="break-label">Break</BreakSessionLabel>
+      <BreakSessionTime id="break-length">
+        {breakLengthInMinutes}
+      </BreakSessionTime>
+      <PlusMinusButtonContainer>
+        <PlusMinusButton
+          id="break-decrement"
+          onClick={decrementBreakLengthByOneMinute}
+        >
+          -
+        </PlusMinusButton>
+        <PlusMinusButton
+          id="break-increment"
+          onClick={incrementBreakLengthByOneMinute}
+        >
+          +
+        </PlusMinusButton>
+      </PlusMinusButtonContainer>
+    </BreakSessionContainer>
+  );
+};
+
+type Props = {
+  breakLength: number
+  decrementBreakLengthByOneMinute: () => void
+  incrementBreakLengthByOneMinute: () => void
+}
+
+export default Break;
---
+++ src/components/Session.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import moment from "moment";
+import { BreakSessionContainer } from "../ui/BreakSessionUi";
+import { BreakSessionLabel } from "../ui/BreakSessionUi";
+import { BreakSessionTime } from "../ui/BreakSessionUi";
+import { PlusMinusButtonContainer } from "../ui/BreakSessionUi";
+import { PlusMinusButton } from "../ui/BreakSessionUi";
+
+const Session: React.FC<Props> = (props) => {
+  const {
+    sessionLength,
+    decrementSessionLengthByOneMinute,
+    incrementSessionLengthByOneMinute,
+  } = props;
+
+  const sessionLengthInMinutes = moment
+    .duration(sessionLength, "s")
+    .asMinutes();
+  return (
+    <BreakSessionContainer>
+      <BreakSessionLabel id="session-label">Session</BreakSessionLabel>
+      <BreakSessionTime id="session-length">
+        {sessionLengthInMinutes}
+      </BreakSessionTime>
+      <PlusMinusButtonContainer>
+        <PlusMinusButton
+          id="session-decrement"
+          onClick={decrementSessionLengthByOneMinute}
+        >
+          -
+        </PlusMinusButton>
+        <PlusMinusButton
+          id="session-increment"
+          onClick={incrementSessionLengthByOneMinute}
+        >
+          +
+        </PlusMinusButton>
+      </PlusMinusButtonContainer>
+    </BreakSessionContainer>
+  );
+};
+
+type Props = {
+  sessionLength: number
+  decrementSessionLengthByOneMinute: () => void
+  incrementSessionLengthByOneMinute: () => void
+}
+
+export default Session;
